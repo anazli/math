ceae2f797b1900f33342bdf10cca7b8bb9235f15
Andreas Nazlidis <andreas221b@gmail.com>
2026-01-29 20:03:05 +0100

    fix ortho class

--- src/orthonormal.h
+++ src/orthonormal.h
@@ -1,27 +1,27 @@
 #pragma once
 
-#include "tools/vec3.h"
+#include "vec3.h"
 
 class OrthoNormalBasis {
  public:
   OrthoNormalBasis() = default;
-  Vec3D u() const { return m_u; }
-  Vec3D v() const { return m_v; }
-  Vec3D w() const { return m_w; }
+  Vec3f u() const { return m_u; }
+  Vec3f v() const { return m_v; }
+  Vec3f w() const { return m_w; }
 
-  Vec3D local(float a, float b, float c) const {
+  Vec3f local(float a, float b, float c) const {
     return a * m_u + b * m_v + c * m_w;
   }
 
-  Vec3D local(const Vec3D& a) const {
+  Vec3f local(const Vec3f& a) const {
     return a.x() * m_u + a.y() * m_v + a.z() * m_w;
   }
 
-  void buildFromW(const Vec3D& w) {
-    auto unit_w = getUnitVectorOf(w);
+  void buildFromW(const Vec3f& w) {
+    auto unit_w = normalized(w);
     auto a =
-        (fabs(unit_w.x()) > 0.9f) ? Vec3D(0.f, 1.f, 0.f) : Vec3D(1.f, 0.f, 0.f);
-    auto v = getUnitVectorOf(cross(unit_w, a));
+        (fabs(unit_w.x()) > 0.9f) ? Vec3f(0.f, 1.f, 0.f) : Vec3f(1.f, 0.f, 0.f);
+    auto v = normalized(cross(unit_w, a));
     auto u = cross(unit_w, v);
     m_u = u;
     m_v = v;
@@ -29,7 +29,7 @@ class OrthoNormalBasis {
   }
 
  private:
-  Vec3D m_u;
-  Vec3D m_v;
-  Vec3D m_w;
+  Vec3f m_u;
+  Vec3f m_v;
+  Vec3f m_w;
 };
